8d2161eb3c9a4078c6b28d56a1a40bc7f9181530
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-10-21 08:47:22 -0700

    create initial project files

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meal-tracker',
+  template: `
+  <div class="container">
+    <h1>My First Angular2 App</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
